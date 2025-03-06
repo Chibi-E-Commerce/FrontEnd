ebdc342e299e0bbbb0368c0129215044563a71c3
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-03-05 22:13:19 -0300

    Pequeno erro arrumado. :)

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState} from 'react'
 
-const UserContext = createContext()
+export const UserContext = createContext()
 
 export function UserProvider({ children }) {
     const [user, setUser] = useState(null)
